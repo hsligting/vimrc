4462115c28c0912d5ae130beafb8e8d1835d20b5
Harrison Sligting <hsligting@icloud.com>
2025-09-08 13:51:37 -0600

    add nord theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,18 @@
 return {
-    {
-    	"EdenEast/nightfox.nvim",
-    	-- config = function()
-    	-- 	vim.cmd("colorscheme dayfox")
-    	-- end,
-    },
-    {
-    	"rose-pine/neovim",
-    	name = "rose-pine",
-    	-- config = function()
-    	-- 	vim.cmd("colorscheme rose-pine-dawn")
-    	-- end,
-    },
+	{
+		"EdenEast/nightfox.nvim",
+		-- config = function()
+		-- 	vim.cmd("colorscheme dayfox")
+		-- end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		-- config = function()
+		-- 	vim.cmd("colorscheme rose-pine-dawn")
+		-- end,
+	},
+	{
+		"nordtheme/vim",
+	},
 }
